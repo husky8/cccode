aa8a9a3c90acf9820d254cc64674fd741cff5378
husky8 <ccheng8@sina.com>
2019-02-27 10:21:15 +0800

    数据更新

--- tools/getSomething.py
+++ tools/getSomething.py
@@ -22,8 +22,19 @@ def getIpAddress(ip):
     :return:
     """
     r=requests.get("http://ip.taobao.com/service/getIpInfo.php?ip="+ip)
-    print(r.text)
+    # print(r.text)
     return eval(r.text)["data"]
 
+def getDateProperty(date):
+    """
+    support :https://www.nowapi.com/api/life.workday
+    :param date: date  yyyymmdd
+    :return: Property
+    ps  "worknm": 假日/工作日,  "week_1": "4", /*星期样式1*/
+    """
+    r=requests.get("http://api.k780.com/?app=life.workday&date={}&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json".format(date))
+    return eval(r.text)["result"]
 if __name__ == '__main__':
-    getIpAddress("36.110.48.247")
+
+    s = getDateProperty("20190203")
+    print(s["worknm"])
---
+++ tools/judgeSometing.py
@@ -0,0 +1,6 @@
+
+def isWorkday(date):
+    """
+    :param date: 要判断的日期
+    :return:
+    """
